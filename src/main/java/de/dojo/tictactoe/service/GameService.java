4708d56ca55ecc8493862f043f9f11eaf8a26824
Steffets <stefan@sol4web.de>
2025-01-27 14:10:37 +0100

    easy done

--- src/main/java/de/dojo/tictactoe/service/GameService.java
+++ src/main/java/de/dojo/tictactoe/service/GameService.java
@@ -5,8 +5,40 @@ import org.springframework.stereotype.Service;
 @Service
 public class GameService {
 
-    public void makeTurn() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public boolean makeTurn(int[][] field, int player, int x, int y) {
+        if (field[x][y] != 0) {
+            return false;
+        }
+        field[x][y] = player;
+        return true;
     }
 
+    public boolean checkWin(int[][] field, int player) {
+        if (
+                field[0][0] == player && field[1][0] == player && field[2][0] == player) {
+            return true;
+        } else  if (
+                field[0][1] == player && field[1][1] == player && field[2][1] == player) {
+            return true;
+        } else  if (
+                field[0][2] == player && field[1][2] == player && field[2][2] == player) {
+            return true;
+        } else if ( // waagrecht
+                field[0][0] == player && field[0][1] == player && field[0][2] == player) {
+            return true;
+        } else  if (
+                field[1][0] == player && field[1][1] == player && field[1][2] == player) {
+            return true;
+        } else  if (
+                field[2][0] == player && field[2][1] == player && field[2][2] == player) {
+            return true;
+        } else  if ( // diagonal
+                field[0][0] == player && field[1][1] == player && field[2][2] == player) {
+            return true;
+        } else  if ( // diagonal
+                field[0][2] == player && field[1][1] == player && field[2][0] == player) {
+            return true;
+        }
+        return false;
+    }
 }
--- src/test/java/de/dojo/tictactoe/service/GameServiceTest.java
+++ src/test/java/de/dojo/tictactoe/service/GameServiceTest.java
@@ -12,12 +12,158 @@ class GameServiceTest {
     @Test
     void makeTurnOnFreeField() {
         // given - row/column etc.
+        int[][] field = new int[3][3];
+        // when;
+        gameService.makeTurn(field, 1, 0, 0 );
+
+        // then
+        Assertions.assertThat(field[0][0]).isEqualTo(1);
+    }
+
+    @Test
+    void makeTurnOnNotFreeField() {
+        // given - row/column etc.
+        int[][] field = new int[3][3];
+        gameService.makeTurn(field, 1, 0, 0 );
 
         // when;
-        gameService.makeTurn();
+        boolean success = gameService.makeTurn(field, 1, 0, 0 );
 
-        // then - Assertions.assertThat()
-
+        // then
+        Assertions.assertThat(success).isFalse();
     }
+
+
+    @Test
+    void noWin() {
+        // given - row/column etc.
+        int[][] field = new int[3][3];
+
+        // when;
+        boolean win = gameService.checkWin(field, 1);
+
+        // then
+        Assertions.assertThat(win).isFalse();
+    }
+
+    @Test
+    void win123() {
+        // given - row/column etc.
+        int[][] field = new int[3][3];
+        gameService.makeTurn(field, 1, 0, 0 );
+        gameService.makeTurn(field, 1, 1, 0 );
+        gameService.makeTurn(field, 1, 2, 0 );
+
+        // when;
+        boolean win = gameService.checkWin(field, 1);
+
+        // then
+        Assertions.assertThat(win).isTrue();
+    }
+
+    @Test
+    void win456() {
+        // given - row/column etc.
+        int[][] field = new int[3][3];
+        gameService.makeTurn(field, 1, 0, 1 );
+        gameService.makeTurn(field, 1, 1, 1 );
+        gameService.makeTurn(field, 1, 2, 1 );
+
+        // when;
+        boolean win = gameService.checkWin(field, 1);
+
+        // then
+        Assertions.assertThat(win).isTrue();
+    }
+
+    @Test
+    void win789() {
+        // given - row/column etc.
+        int[][] field = new int[3][3];
+        gameService.makeTurn(field, 1, 0, 2 );
+        gameService.makeTurn(field, 1, 1, 2 );
+        gameService.makeTurn(field, 1, 2, 2 );
+
+        // when;
+        boolean win = gameService.checkWin(field, 1);
+
+        // then
+        Assertions.assertThat(win).isTrue();
+    }
+
+    @Test
+    void win136() {
+        // given - row/column etc.
+        int[][] field = new int[3][3];
+        gameService.makeTurn(field, 1, 0, 0 );
+        gameService.makeTurn(field, 1, 0, 1 );
+        gameService.makeTurn(field, 1, 0, 2 );
+
+        // when;
+        boolean win = gameService.checkWin(field, 1);
+
+        // then
+        Assertions.assertThat(win).isTrue();
+    }
+
+    @Test
+    void win248() {
+        // given - row/column etc.
+        int[][] field = new int[3][3];
+        gameService.makeTurn(field, 1, 1, 0 );
+        gameService.makeTurn(field, 1, 1, 1 );
+        gameService.makeTurn(field, 1, 1, 2 );
+
+        // when;
+        boolean win = gameService.checkWin(field, 1);
+
+        // then
+        Assertions.assertThat(win).isTrue();
+    }
+
+    @Test
+    void win359() {
+        // given - row/column etc.
+        int[][] field = new int[3][3];
+        gameService.makeTurn(field, 1, 2, 0 );
+        gameService.makeTurn(field, 1, 2, 1 );
+        gameService.makeTurn(field, 1, 2, 2 );
+
+        // when;
+        boolean win = gameService.checkWin(field, 1);
+
+        // then
+        Assertions.assertThat(win).isTrue();
+    }
+
+    @Test
+    void win059() {
+        // given - row/column etc.
+        int[][] field = new int[3][3];
+        gameService.makeTurn(field, 1, 0, 0 );
+        gameService.makeTurn(field, 1, 1, 1 );
+        gameService.makeTurn(field, 1, 2, 2 );
+
+        // when;
+        boolean win = gameService.checkWin(field, 1);
+
+        // then
+        Assertions.assertThat(win).isTrue();
+    }
+    @Test
+    void win357() {
+        // given - row/column etc.
+        int[][] field = new int[3][3];
+        gameService.makeTurn(field, 1, 2, 0 );
+        gameService.makeTurn(field, 1, 1, 1 );
+        gameService.makeTurn(field, 1, 0, 2 );
+
+        // when;
+        boolean win = gameService.checkWin(field, 1);
+
+        // then
+        Assertions.assertThat(win).isTrue();
+    }
+
 
 }
